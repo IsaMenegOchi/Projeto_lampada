e1728aee5b5b8b82cb4a4cb4448d19f35e38cd46
IsaMenegOchi <isabelle.ochini@gmail.com>
2021-08-31 09:19:16 -0300

    Adicionando botão e ação piscar

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict" //fica vigiando o que faço de errado
 
+const lampada = document.getElementById("lampada")
+let idInterval
 
 //FUNÇÕES
 
@@ -11,43 +13,88 @@ function buttonOnOff (ligarState, desligarState) {
     ligar.disabled = desligarState
 }
 
-function lampadaQuebrada() {
-    return document.getElementById("lampada").
-    src.indexOf("quebrada") !== -1
-}
+//DESLIGAR BOTÃO
 
 function ligarLampada() {
-    const lampada = document.getElementById("lampada")
 
-    if(!lampadaQuebrada()){
+    if(!quebrarLampada()){
         lampada.src = "img/ligada.jpg"
         buttonOnOff(false, true)
     }
 }
 
+//DESLIGAR BOTÃO
+
 function desligarLampada() {
-    const lampada = document.getElementById("lampada")
 
-
-    if(!lampadaQuebrada()){
-
+    if(!quebrarLampada()){
         lampada.src = "img/desligada.jpg"
         buttonOnOff(true, false)
     }
 }
 
+
+
+//QUEBRAR LAMPADA
+
+// function lampadaQuebrada() {
+//     return document.getElementById("lampada").
+//     src.indexOf("quebrada") !== -1
+// }
+
+//FUNCTION ARROW
+const lampadaQuebrada = () => document.getElementById("lampada").src.indexOf("quebrada") !== -1
+
+
 function quebrarLampada() {
-    const lampada = document.getElementById("lampada")
-    
     lampada.src = "img/quebrada.jpg"
     buttonOnOff(true, true)
 }
+
+
+//PISCAR LAMPADA
+
+//FUNCTION ARROW
+const lampadaDesligada = () => lampada.src.includes("desligada")
+
+//função normal
+// function lampadaDesligada (){
+//     return lampada.src.includes("desligada")
+// }
+
+
+function trocarImagem(){
+    if (lampadaDesligada()){
+        ligarLampada()
+    }
+    else{
+        desligarLampada()
+    }
+}
+
+
+function piscarLampada(){
+
+    const piscar = document.getElementById("piscar")
+    
+
+    if(piscar.textContent == "Piscar"){
+        idInterval = setInterval(trocarImagem, 1000);
+        piscar.textContent = "Parar"
+    }
+    else{
+        clearInterval(idInterval)
+        piscar.textContent = "Piscar"
+    }
+}
+ 
 
 
 
 //EVENTO
 document.getElementById("ligar").addEventListener("click", ligarLampada)
 document.getElementById("desligar").addEventListener("click", desligarLampada)
+document.getElementById("piscar").addEventListener("click", piscarLampada )
 
 document.getElementById("lampada").addEventListener("dblclick", quebrarLampada)
 document.getElementById("lampada").addEventListener("mouseover", ligarLampada)
